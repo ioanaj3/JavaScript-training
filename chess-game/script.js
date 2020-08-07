053dbe9702a2bc74fd1d2cc2b1b0148623d41211
Ioana Jitariu <ioana.jitariu@bitstone.eu>
2020-08-07 11:09:37 +0300

    Added countdown

--- chess-game/script.js
+++ chess-game/script.js
@@ -1,23 +1,21 @@
-// COUNTDOWN - uncomment to activate
 
-// let $countdown_number = $('<span/>');
-// $countdown_number.text("5")
-//                  .attr('id', "counter-number")
-//                  .appendTo($("body"))
+let $countdown_number = $('<span/>');
+$countdown_number.text("5")
+                 .attr('id', "counter-number")
+                 .appendTo($("body"))
 
 
-// let newNumber = parseInt($countdown_number.text())
-// function countdown(){   
-//         newNumber = newNumber - 1;
-//         $countdown_number.text(newNumber);
-//         if (newNumber > 0) 
-//             setTimeout(countdown,1000);
-//         else{
-//             $countdown_number.remove()
-//             let base_table = new ChessTable()
+let newNumber = parseInt($countdown_number.text())
+function countdown(){   
+        newNumber = newNumber - 1;
+        $countdown_number.text(newNumber);
+        if (newNumber > 0) 
+            setTimeout(countdown,500);
+        else{
+            $countdown_number.remove()
+            let base_table = new ChessTable()
 
-//         }
-//  }
-// setTimeout(countdown,1000)
+        }
+ }
+setTimeout(countdown,500)
 
-let base_table = new ChessTable()
